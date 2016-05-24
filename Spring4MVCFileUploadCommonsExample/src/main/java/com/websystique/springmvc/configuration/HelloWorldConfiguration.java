9649c2d99d4fd361a1d8fa8d7ab016ec1eb961ee
Jitender <jittuchahar@hotmail.com>
2016-05-25 01:04:13 +0530

    First Commit
    Committing code base file for multi-part upload

---
+++ Spring4MVCFileUploadCommonsExample/src/main/java/com/websystique/springmvc/configuration/HelloWorldConfiguration.java
@@ -0,0 +1,56 @@
+package com.websystique.springmvc.configuration;
+
+import java.io.IOException;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.websystique.springmvc")
+public class HelloWorldConfiguration extends WebMvcConfigurerAdapter{
+
+	@Bean(name="multipartResolver") 
+	public CommonsMultipartResolver getResolver() throws IOException{
+		CommonsMultipartResolver resolver = new CommonsMultipartResolver();
+		
+		//Set the maximum allowed size (in bytes) for each individual file.
+		resolver.setMaxUploadSizePerFile(5242880);//5MB
+		
+		//You may also set other available properties.
+		
+		return resolver;
+	}
+
+	@Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+        registry.viewResolver(viewResolver);
+    }
+    
+	@Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("messages");
+        return messageSource;
+    }
+	
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("/static/");
+    }
+
+}
